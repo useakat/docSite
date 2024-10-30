05f8d8fe086cb8de1ee67a2e3252a4919ae57ea0
useakat <useakat@gmail.com>
2024-10-30 23:40:58 +0900

    dist folder created

---
+++ dist/_astro/hoisted.6119e170.js
@@ -0,0 +1,1 @@
+import"https://identity.netlify.com/v1/netlify-identity-widget.js";window.netlifyIdentity&&window.netlifyIdentity.on("init",i=>{i||window.netlifyIdentity.on("login",()=>{document.location.href="/admin/"})});
